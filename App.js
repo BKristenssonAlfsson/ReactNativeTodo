e973e37355334f1193368f25cb8de963040df811
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-04-03 09:30:26 +0200

    MVP in React Native

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 
 export default function App() {
 
   const [enteredGoal, setEnteredGoal] = useState('');
-  
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput placeholder="Enter New Goal" 
-              style={styles.input} />
-        <Button title="ADD"/>
+              style={styles.input} 
+              onChangeText={goalInputHandler}
+              value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
